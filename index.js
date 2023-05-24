9fe44844c26a16b1a40371c513c90c5e58901001
Kunal Verma <kunalvrm555@gmail.com>
2023-05-24 11:00:03 +0530

    setup basic express server

---
+++ index.js
@@ -0,0 +1,25 @@
+// Importing required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Create an instance of Express
+const app = express();
+const port = 3000;
+
+// Configure middleware
+app.use(bodyParser.json());
+
+// Define GET endpoint at base URL '/'
+app.get('/', (req, res) => {
+    res.send("Got a GET request at '/'");
+});
+
+// Define POST endpoint at base URL '/'
+app.post('/', (req, res) => {
+    res.send("Got a POST request at '/'");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log("Listening on PORT 3000");
+});
